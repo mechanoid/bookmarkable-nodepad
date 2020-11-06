cce020d7bbb31e5184b1c7faa1d05d2a0803f684
Falk Hoppe <falkhoppe81@gmail.com>
2020-11-06 10:55:54 +0100

    fix dist file naming issues

--- bin/bookmarkable-notepad.js
+++ bin/bookmarkable-notepad.js
@@ -4,7 +4,7 @@ import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const bookmarklet = await fs.readFile(resolve(__dirname, '..','dist', 'bookmarkeble-notepad.bookmarklet'))
+const bookmarklet = await fs.readFile(resolve(__dirname, '..','dist', 'bookmarkable-notepad.bookmarklet'))
 
 console.log(`Copy the following line to your browsers address bar, and save it as bookmark:
 ---`);
